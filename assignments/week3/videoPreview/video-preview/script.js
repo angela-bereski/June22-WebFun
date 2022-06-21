801dba443a03f5303ee5e6d0f6d370258c617fca
angela bereski <angelakbereski@gmail.com>
2022-06-21 12:47:34 -0600

    video preview

---
+++ assignments/week3/videoPreview/video-preview/script.js
@@ -0,0 +1,12 @@
+console.log("page loaded...");
+
+
+function over(element) {
+    sunsetvid.play();    
+    console.log("played video");
+}
+
+function out(element) {
+    sunsetvid.pause();   
+    console.log("paused video");
+}
